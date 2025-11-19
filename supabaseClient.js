eaee63d0898fd6b26267be0f2a8526420a011c96
thapaharsh940-ops <thapaharsh940@gmail.com>
2025-11-19 12:43:25 +0530

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,10 @@
-// Replace these with your actual Supabase project credentials
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://gcojuhkujvwuffzwuypt.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)  
+
+
 const SUPABASE_URL = 'https://gcojuhkujvwuffzwuypt.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdjb2p1aGt1anZ3dWZmend1eXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxOTY4ODgsImV4cCI6MjA3ODc3Mjg4OH0.v9dNR9qXETtrqjxdvHLqrAWXjrF-6Aw36F6Ky1YSLdM';
 
