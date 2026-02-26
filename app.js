cc76e2c164264182188c549860b43d2f178de761
thapaharsh940-ops <thapaharsh940@gmail.com>
2026-02-26 16:14:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 const SUPABASE_URL = 'https://gcojuhkujvwuffzwuypt.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdjb2p1aGt1anZ3dWZmend1eXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxOTY4ODgsImV4cCI6MjA3ODc3Mjg4OH0.v9dNR9qXETtrqjxdvHLqrAWXjrF-6Aw36F6Ky1YSLdM';
 
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
 const ADMIN_CODE = "sahil12345"; 
 
 let currentUser = null;
